ca16edd7a0e612042c5d1ece5fe7f74a0d6ebaa1
Tamas Mohos <zerosuxx@gmail.com>
2021-10-18 13:20:22 +0200

    add directors table migration

---
+++ database/migrations/2021_10_11_133030_create_directors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDirectorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('directors', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('tmdb_id');
+            $table->unique('tmdb_id');
+            $table->string('name');
+            $table->text('biography');
+            $table->date('date_of_birth')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('directors');
+    }
+}
